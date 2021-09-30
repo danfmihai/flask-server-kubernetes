9b982680d2981f0e32bf5ee848956d12eddab55a
mihai <misu3108@yahoo.com>
2021-09-30 03:46:26 +0000

    edited

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+kubectl create namespace flaskapp
+
+kubectl create -f .
+sleep 2
+kubectl get -n flaskapp pods
+kubectl get -n flaskapp svc
+sleep 2
+pod_name=$(kubectl get -n flaskapp pods | grep -i myflaskapp | awk '{print $1}')
+
+kubectl logs -n flaskapp $pod_name
+
+
